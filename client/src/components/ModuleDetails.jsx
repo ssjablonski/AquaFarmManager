34c6ff59e91753c8191df2dcc5024694f342cf41
Sebastian Jablonski <sebaa1080@gmail.com>
2024-07-14 15:38:21 +0200

    Better styling in ModuleDetails component + updated manifest.json

--- client/src/components/ModuleDetails.jsx
+++ client/src/components/ModuleDetails.jsx
@@ -1,4 +1,3 @@
-// src/components/ModuleDetails.js
 import React from "react";
 
 const ModuleDetails = ({ module, currentTemp, onEditClick }) => {
@@ -8,36 +7,39 @@ const ModuleDetails = ({ module, currentTemp, onEditClick }) => {
         : "bg-red-100 text-red-500 dark:bg-red-200 dark:text-red-600";
 
     return (
-        <div
-            className={`container mx-auto my-6 flex max-w-5xl flex-col items-center justify-center gap-4 rounded-lg bg-gray-200 p-4 dark:bg-black-400 dark:text-white`}
-        >
+        <div className="container mx-auto my-6 flex flex-col items-center justify-center gap-4 rounded-lg bg-gray-200 p-4 dark:bg-black-400 dark:text-white xl:max-w-5xl">
             <h1 className="mb-4 text-center text-4xl font-semibold">{module.name}</h1>
-            <div className="container mx-auto flex w-2/3 flex-col justify-between gap-2 rounded-xl bg-gray-100 p-4 dark:bg-black-300 md:flex-row">
+            <div className="container mx-auto flex w-full min-w-60 flex-col gap-2 rounded-xl bg-gray-100 p-4 dark:bg-black-300 lg:flex-row">
                 {module.available ? (
                     <div
-                        className={`container mx-auto flex flex-col items-center justify-around rounded-lg p-12 text-3xl font-bold md:max-w-56 ${tempColorClass}`}
+                        className={`mx-auto flex min-h-52 flex-col items-center justify-around rounded-lg p-12 text-3xl font-bold ${tempColorClass} w-full lg:w-1/2`}
                     >
                         <p className="text-5xl">Actual:</p>
                         <p className="text-center text-5xl">{currentTemp}°C</p>
                     </div>
                 ) : (
-                    <div className="container mx-auto flex flex-col items-center justify-around rounded-lg bg-gray-300 p-12 text-3xl font-bold dark:bg-black-200 md:max-w-56">
+                    <div className="mx-auto flex min-h-52 w-full min-w-5 flex-col items-center justify-around rounded-lg bg-gray-300 p-12 text-3xl font-bold dark:bg-black-200 lg:w-1/2">
                         <p className="text-5xl">Actual:</p>
-                        <p className="text-center text-5xl">{currentTemp}°C</p>
+                        <p className="text-center text-5xl">-°C</p>
                     </div>
                 )}
-                <div className="text-black container mx-auto flex flex-col items-center justify-around rounded-lg bg-white p-12 text-3xl font-bold dark:bg-black-200 dark:text-white md:max-w-56">
+                <div className="text-black mx-auto flex min-h-52 w-full min-w-5 flex-col items-center justify-around rounded-lg bg-white p-12 text-3xl font-bold dark:bg-black-200 dark:text-white lg:w-1/2">
                     <p className="text-5xl">Target:</p>
                     <p className="text-center text-5xl">{module.targetTemperature}°C</p>
                 </div>
             </div>
-            <div className="container mx-auto flex w-2/3 justify-center rounded-lg bg-red-200 p-4 text-3xl dark:bg-black-300">
+            <div className="container mx-auto flex w-full justify-center rounded-lg bg-gray-100 p-4 text-xl dark:bg-black-300 lg:text-2xl">
                 <p>{module.description}</p>
             </div>
+            <p
+                className={`text-center text-4xl font-semibold ${module.available ? "text-green-500" : "text-red-500"}`}
+            >
+                {module.available ? "AVAILABLE" : "NOT AVAILABLE"}
+            </p>
             <div className="flex items-center justify-center">
                 <button
                     disabled={!module.available}
-                    className={`mt-4 justify-center rounded-lg p-2 px-8 text-xl ${
+                    className={`mt-2 justify-center rounded-lg p-2 px-8 text-xl ${
                         module.available
                             ? "bg-green-500 text-white hover:bg-green-300"
                             : "cursor-not-allowed bg-gray-300 text-gray-500"
